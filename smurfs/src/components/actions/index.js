890b7115ef5f1365e8893059a7e9a3eeb1828fca
adkhiker <poirier.greg@gmail.com>
2019-11-24 21:49:09 -0500

    WIP: Smurf form

---
+++ smurfs/src/components/actions/index.js
@@ -0,0 +1,18 @@
+export {
+    FETCH_SMURF_DATA,
+    POST_SMURF_DATA,
+    POST_SMURF_DATA_FAILED,
+    POST_SMURF_DATA_SUCCESS,
+    DELETE_SMURF,
+    FETCH_SMURF_DATA_FAILED,
+    FETCH_SMURF_DATA_SUCCESS,
+    DELETE_SMURF_FAILED,
+    DELETE_SMURF_SUCCESS,
+    UPDATE_SMURF,
+    UPDATE_SMURF_FAILED,
+    UPDATE_SMURF_SUCCESS,
+    updateSmurf,
+    postData,
+    getData,
+    deleteSmurf
+} from './smurfActions';
